d3a837992254c2786f5a7dea85438ba65acae007
Yash Vekaria <30694521+Yash-Vekaria@users.noreply.github.com>
2022-05-06 05:38:04 -0700

    Update train.sh

--- train.sh
+++ train.sh
@@ -8,11 +8,9 @@ sudo sysctl -w net.ipv4.tcp_congestion_control=cubic
 
 # configuring traffic controller (tc) to add a new queuing discipline (qdisc) (it actually replaces the default): tbf to localhost (lo) interface with given rate.
 # tbf (token bucket filter): Outgoing (egress) traffic bytes is serviced by a single token. These tokens are stored in a bucket of limited size and refreshed at the desired output rate.
-sudo tc qdisc add dev lo root handle 1: tbf rate 1000kbit buffer 
-
 # transmit the bursts at a peakrate (rate at which buffer empties) of 1001kbit and minimum burst (buffer) of 1550 bytes to be sent at maximum rate, so a typical packet can be sent at once (MTU = 1500).
 # ref: https://man7.org/linux/man-pages/man8/tc-tbf.8.html
-10000000 limit 500000000 peakrate 1001kbit minburst 1550
+sudo tc qdisc add dev lo root handle 1: tbf rate 1000kbit buffer 10000000 limit 500000000 peakrate 1001kbit minburst 1550
 
 # delay packets from a uniform distribution with 100ms +- 10ms (i.e., 90ms-110ms) using netem (network emulation)
 sudo tc qdisc add dev lo parent 1:1 handle 10: netem delay 100ms 10ms
