c36065f3587d0503b7a1034552a299065cbfa190
Yash Vekaria <30694521+Yash-Vekaria@users.noreply.github.com>
2022-05-09 00:53:17 -0700

    Update receiver.py

--- receiver.py
+++ receiver.py
@@ -23,6 +23,8 @@ sock.bind((IP_ADDRESS, PORT))
 pointer = list(range(1, int(RWND)+1))
 # Maintains the highest sequence number received
 highest_cumulative_sequence = 1
+sent_acknowledgements = []
+unacknowledged_sequences = []
 
 # Receiver keeps running indefinitely to receive the data
 while True:
@@ -41,20 +43,29 @@ while True:
 
     if int(seq) == int(pointer[0]):
         # This condition will be true only when inclusion of current seq being received makes all sequences uptil highest_cumulative_sequence as received 
-        if highest_cumulative_sequence + 1 == pointer[1]:
+        if int(highest_cumulative_sequence) + 1 == pointer[1]:
             print("Sending Acknowledgement #", highest_cumulative_sequence)
-            sock.sendto(highest_cumulative_sequence.encode(), sender_address)
+            sent_acknowledgements.append(int(highest_cumulative_sequence))
+            sock.sendto(str(highest_cumulative_sequence).encode(), sender_address)
         else:
-            print("Sending Acknowledgement #", seq)
-            sock.sendto(seq.encode(), sender_address)
+            unacknowledged_sequences = sorted(list(set(range(1, int(highest_cumulative_sequence)+1)).difference(set(sent_acknowledgements))))
+            if int(unacknowledged_sequences[0]) < int(highest_cumulative_sequence):
+                print("Sending Acknowledgement #", int(unacknowledged_sequences[0]))
+                sent_acknowledgements.append(int(unacknowledged_sequences[0]))
+                sock.sendto(str(int(unacknowledged_sequences[0])).encode(), sender_address)
+            else:
+                print("Sending Acknowledgement #", seq)
+                sent_acknowledgements.append(int(seq))
+                sock.sendto(seq.encode(), sender_address)
 
     else:
         # Else handles the case when a packet is lost. 
         # In such case, the sequence number of last continuous packet received is sent
         print("Sending Acknowledgement #", pointer[0] - 1)
+        sent_acknowledgements.append(int(pointer[0] - 1))
         sock.sendto(str(pointer[0] - 1).encode(), sender_address)
         # Checks if current sequence number is higher than the highest sequence number already receives? Updates if it is
-        if seq > highest_cumulative_sequence:
+        if int(seq) > highest_cumulative_sequence:
             highest_cumulative_sequence = seq
     
     # Removing the received sequence number from pointer list
