c581a62ff69bb43fcbfe3576095011389160db69
Yash Vekaria <30694521+Yash-Vekaria@users.noreply.github.com>
2022-05-09 00:15:51 -0700

    Update receiver.py

--- receiver.py
+++ receiver.py
@@ -21,6 +21,8 @@ sock.bind((IP_ADDRESS, PORT))
 
 # Implements cumulative acknowledgement by maintaining the next sequence it expects at position 0. It removes all recived sequences.
 pointer = list(range(1, int(RWND)+1))
+# Maintains the highest sequence number received
+highest_cumulative_sequence = 1
 
 # Receiver keeps running indefinitely to receive the data
 while True:
@@ -38,13 +40,22 @@ while True:
 
 
     if int(seq) == int(pointer[0]):
-        print("Sending Acknowledgement #", seq)
-        sock.sendto(seq.encode(), sender_address)
+        # This condition will be true only when inclusion of current seq being received makes all sequences uptil highest_cumulative_sequence as received 
+        if highest_cumulative_sequence + 1 == pointer[1]:
+            print("Sending Acknowledgement #", highest_cumulative_sequence)
+            sock.sendto(highest_cumulative_sequence.encode(), sender_address)
+        else:
+            print("Sending Acknowledgement #", seq)
+            sock.sendto(seq.encode(), sender_address)
+
     else:
         # Else handles the case when a packet is lost. 
         # In such case, the sequence number of last continuous packet received is sent
         print("Sending Acknowledgement #", pointer[0] - 1)
         sock.sendto(str(pointer[0] - 1).encode(), sender_address)
+        # Checks if current sequence number is higher than the highest sequence number already receives? Updates if it is
+        if seq > highest_cumulative_sequence:
+            highest_cumulative_sequence = seq
     
     # Removing the received sequence number from pointer list
     if int(seq) in pointer:
